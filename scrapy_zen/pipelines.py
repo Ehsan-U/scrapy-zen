f2f0401e8c1b080f9329479d6695c60b099930fd
Ehsan U. <lancerf562@gmail.com>
2025-02-16 19:19:03 +0500

    feat: add gRPC pipeline

--- scrapy_zen/pipelines.py
+++ scrapy_zen/pipelines.py
@@ -1,6 +1,8 @@
 from functools import wraps
+import importlib
 import json
-from typing import Dict, Callable, List
+from typing import Dict, Callable, List, Self
+import grpc
 import scrapy
 from scrapy.crawler import Crawler
 from scrapy.spiders import Spider
@@ -10,6 +12,8 @@ from datetime import datetime, timedelta
 from scrapy.utils.defer import maybe_deferred_to_future
 from scrapy.http.request import NO_CALLBACK
 import dateparser
+from twisted.internet.threads import deferToThread
+from twisted.internet.defer import Deferred
 
 
 
@@ -18,30 +22,30 @@ class PreProcessingPipeline:
     Pipeline to preprocess items before forwarding.
     Handles deduplication, date filtering, and data cleaning.
 
-    Args:
+    Attributes:
         file_path (str): Path to TinyDB database file. Defaults to "db.json"
         expiry_days (int): Number of days to keep records. Defaults to 60
     """
 
-    def __init__(self, file_path: str = "db.json", expiry_days: int = 60):
+    def __init__(self, file_path: str = "db.json", expiry_days: int = 60) -> None:
         self.file_path = file_path
         self.expiry_days = expiry_days
 
     @classmethod
-    def from_crawler(cls, crawler: Crawler):
+    def from_crawler(cls, crawler: Crawler) -> Self:
         return cls(
             file_path=crawler.settings.get("PREPROCESSING_DB_PATH"),
             expiry_days=crawler.settings.get("PREPROCESSING_EXPIRY_DAYS"),
         )
 
-    def open_spider(self, spider: Spider):
+    def open_spider(self, spider: Spider) -> None:
         self._init_db()
 
-    def close_spider(self, spider: Spider):
+    def close_spider(self, spider: Spider) -> None:
         if hasattr(self, "db"):
             self._db.close()
 
-    def _init_db(self):
+    def _init_db(self) -> None:
         self._db = TinyDB(self.file_path)
         self._query = Query()
         self._cleanup_old_records()
@@ -81,7 +85,7 @@ class PreProcessingPipeline:
         return wrapper
 
     @clean_item
-    def process_item(self, item: Dict, spider: Spider):
+    def process_item(self, item: Dict, spider: Spider) -> Dict:
         _id = item.get("_id", None)
         if _id:
             if self.tinydb_exists(id=_id):
@@ -99,35 +103,35 @@ class PreProcessingPipeline:
     
 
 
-class DiscordWebhookPipeline:
+class DiscordPipeline:
     """
     Pipeline to send items to a Discord webhook.
 
-    Args:
-        webhook_uri (str): discord webhook uri
+    Attributes:
+        uri (str):
         exclude_fields (List[str]): List of fields that needs to be excluded for this pipeline
     """
     exclude_fields: List[str] = ["body"]
 
-    def __init__(self, webhook_uri: str):
-        self.webhook_uri = webhook_uri
+    def __init__(self, uri: str) -> None:
+        self.uri = uri
 
     @classmethod
-    def from_crawler(cls, crawler):
+    def from_crawler(cls, crawler: Crawler) -> Self:
         return cls(
-            webhook_uri=crawler.settings.get("DISCORD_WEBHOOK_URI")
+            uri=crawler.settings.get("DISCORD_URI")
         )
 
-    async def process_item(self, item: Dict, spider: Spider):
+    async def process_item(self, item: Dict, spider: Spider) -> Dict:
         await self._send(item, spider)
         return item
 
     async def _send(self, item: Dict, spider: Spider) -> None:
-        _item = {k:v for k,v in item.items() if not k.startswith("_") and k not in self.exclude_fields}
+        _item = {k:v for k,v in item.items() if not k.startswith("_") and k.lower() not in self.exclude_fields}
         await maybe_deferred_to_future(
             spider.crawler.engine.download(
                 scrapy.Request(
-                    url=self.webhook_uri,
+                    url=self.uri,
                     method="POST",
                     body=json.dumps(_item),
                     headers={"Content-Type": "application/json"},
@@ -142,33 +146,36 @@ class SynopticPipeline:
     """
     Pipeline to send items to a Synoptic stream.
 
-    Args:
-        stream_id (str): stream ID of synoptic stream
-        api_key (str): api key of synoptic stream
+    Attributes:
+        stream_id (str):
+        api_key (str):
+        exclude_fields (List[str]): List of fields that needs to be excluded for this pipeline
     """
     exclude_fields: List[str] = []
 
-    def __init__(self, stream_id: str, api_key: str):
+    def __init__(self, uri: str, stream_id: str, api_key: str) -> None:
+        self.uri = uri
         self.stream_id = stream_id
         self.api_key = api_key
 
     @classmethod
-    def from_crawler(cls, crawler):
+    def from_crawler(cls, crawler: Crawler) -> Self:
         return cls(
+            uri=crawler.settings.get("SYNOPTIC_URI"),
             stream_id=crawler.settings.get("SYNOPTIC_STREAM_ID"),
             api_key=crawler.settings.get("SYNOPTIC_API_KEY")
         )
 
-    async def process_item(self, item: Dict, spider: Spider):
+    async def process_item(self, item: Dict, spider: Spider) -> Dict:
         await self._send(item, spider)
         return item
 
     async def _send(self, item: Dict, spider: Spider) -> None:
-        _item = {k:v for k,v in item.items() if not k.startswith("_") and k not in self.exclude_fields}
+        _item = {k:v for k,v in item.items() if not k.startswith("_") and k.lower() not in self.exclude_fields}
         await maybe_deferred_to_future(
             spider.crawler.engine.download(
                 scrapy.Request(
-                    url="https://developer.synoptic.com/graphql",
+                    url=self.uri,
                     body=json.dumps(_item),
                     method="POST",
                     headers={"content-type": "application/json", 'x-api-key': self.api_key},
@@ -176,3 +183,112 @@ class SynopticPipeline:
                 )
             )
         )
+
+
+
+class TelegramPipeline:
+    """
+    Pipeline to send items to a Telegram channel.
+
+    Attributes:
+        uri (str):
+        token (str):
+        chat_id (str):
+        exclude_fields (List[str]): List of fields that needs to be excluded for this pipeline
+    """
+    exclude_fields: List[str] = []
+
+    def __init__(self,  uri: str, token: str, chat_id: str) -> None:
+        self.uri = uri
+        self.token = token
+        self.chat_id = chat_id
+
+    @classmethod
+    def from_crawler(cls, crawler: Crawler) -> Self:
+        return cls(
+            uri=crawler.settings.get("TELEGRAM_URI"),
+            token=crawler.settings.get("TELEGRAM_TOKEN"),
+            chat_id=crawler.settings.get("TELEGRAM_CHAT_ID"),
+        )
+
+    async def process_item(self, item: Dict, spider: Spider) -> Dict:
+        await self._send(item, spider)
+        return item
+    
+    async def _send(self, item: Dict, spider: Spider) -> None:
+        _item = {k:v for k,v in item.items() if not k.startswith("_") and k.lower() not in self.exclude_fields}
+        await maybe_deferred_to_future(
+            spider.crawler.engine.download(
+                scrapy.Request(
+                    url=self.uri,
+                    body=json.dumps(_item),
+                    method="POST",
+                    headers={"content-type": "application/json", 'authorization': self.token},
+                    callback=NO_CALLBACK
+                )
+            )
+        )
+
+
+
+class GRPCPipeline:
+    """
+    Pipeline to send items to a gRPC server.
+
+    Attributes:
+        uri (str):
+        token (str):
+        id (str):
+        proto_module (str): dotted path to gRPC contract module
+        exclude_fields (List[str]): List of fields that needs to be excluded for this pipeline
+    """
+    exclude_fields: List[str] = []
+
+    def __init__(self, uri: str, token: str, id: str, proto_module: str) -> None:
+        self.uri = uri
+        self.token = token
+        self.id = id
+        self.feed_pb2 = importlib.import_module(f"{proto_module}.feed_pb2")
+        self.feed_pb2_grpc = importlib.import_module(f"{proto_module}.feed_pb2_grpc")
+        # gRPC channel is thread-safe
+        self._channel_grpc = grpc.secure_channel(self.uri, grpc.ssl_channel_credentials())
+
+    @classmethod
+    def from_crawler(cls, crawler: Crawler) -> Self:
+        return cls(
+            uri=crawler.settings.get("GRPC_URI"),
+            token=crawler.settings.get("GRPC_TOKEN"),
+            id=crawler.settings.get("GRPC_ID"),
+            proto_module=crawler.settings.get("GRPC_PROTO_MODULE")
+        )
+
+    def process_item(self, item: Dict, spider: Spider) -> Deferred:
+        d = self._send(item, spider)
+        return d
+    
+    def _send(self, item: Dict, spider: Spider) -> Deferred:
+        _item = {k:v for k,v in item.items() if not k.startswith("_") and k.lower() not in self.exclude_fields}
+        feed_message = self.feed_pb2.FeedMessage(
+            token=self.token,
+            feedId=self.id,
+            messageId=item['_id'],
+            message=json.dumps(_item)
+        )
+        def _on_success(result) -> Dict:
+            spider.logger.debug(f"Successfully sent to gRPC aggregator: {item['_id']}")
+            return item
+        d = deferToThread(self._submit, feed_message)
+        d.addCallback(_on_success)
+        d.addErrback(lambda f: spider.logger.error(f"Failed to send to gRPC aggregator: {item['_id']}\n{f.value}"))
+        return d
+
+    def _submit(self, feed_message) -> None:
+        try:
+            # gRPC clients are lightweight, don't need to be cached or reused
+            client = self.feed_pb2_grpc.IngressServiceStub(self._channel_grpc)
+            client.SubmitFeedMessage(feed_message)
+        except grpc.RpcError as e:
+            raise e
+
+    def close_spider(self, spider: Spider) -> None:
+        self._channel_grpc.close()
