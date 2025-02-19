1247fc42cdc646b47ef9ce3be46eafc822254b78
Ehsan U. <lancerf562@gmail.com>
2025-02-19 14:35:56 +0500

    feat: add logparser

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ logging.getLogger("urllib3.connectionpool").setLevel(logging.WARNING)
 
 class Book(scrapy.Spider):
     name = "book"
-    start_urls = ['https://books.toscrape.com/']
+    start_urls = ['httpsaa//books.toscrape.com/']
 
     def parse(self, response):
         for book in response.xpath("//article"):
@@ -19,6 +19,17 @@ crawler = CrawlerProcess(settings={
         "scrapy_zen.pipelines.PreProcessingPipeline": 543
     },
     "ADDONS": {"scrapy_zen.addons.SpidermonAddon": 1},
+    "LOG_FILE": "logs.log",
+    "LOG_FILE_APPEND": False
 })
 crawler.crawl(Book)
 crawler.start()
+
+# import logparser
+# from pprint import pprint
+
+# with open("logs.log", 'r') as f:
+#     logs = f.read()
+
+# d = logparser.parse(logs)
+# pprint(d['log_categories']['error_logs']['details'])
