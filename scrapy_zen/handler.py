45cfd8955536c635a384aab480d34f3cc84954af
Ehsan U. <lancerf562@gmail.com>
2025-02-19 09:36:57 +0500

    chores: type hinting

--- scrapy_zen/handler.py
+++ scrapy_zen/handler.py
@@ -2,18 +2,23 @@ from scrapy_playwright.handler import ScrapyPlaywrightDownloadHandler
 from scrapy_zyte_api import ScrapyZyteAPIDownloadHandler
 from scrapy_impersonate import ImpersonateDownloadHandler
 from scrapy.core.downloader.handlers.http import HTTPDownloadHandler
+from twisted.internet.defer import Deferred
+from scrapy.http import Request, Response
+from scrapy import Spider
+from scrapy.crawler import Crawler
+from typing import Self
 
 
 class ZenDownloadHandler:
     
-    def __init__(self, crawler):
+    def __init__(self, crawler: Crawler):
         self.crawler = crawler
 
     @classmethod
-    def from_crawler(cls, crawler):
+    def from_crawler(cls, crawler: Crawler) -> Self:
         return cls(crawler)
 
-    def download_request(self, request, spider):
+    def download_request(self, request: Request, spider: Spider) -> Deferred | Deferred[Response]:
         if request.meta.get('playwright'):
             if not hasattr(self, "playwright_handler"):
                 self.playwright_handler = ScrapyPlaywrightDownloadHandler.from_crawler(self.crawler)
