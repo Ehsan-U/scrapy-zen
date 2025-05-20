e0a4b9721839b1bea8eba8fda0b6b12cc2a49499
Ehsan U. <lancerf562@gmail.com>
2025-05-20 20:10:22 +0500

    fix: aiohttp

--- scrapy_zen/handler.py
+++ scrapy_zen/handler.py
@@ -7,11 +7,12 @@ from scrapy.http import Request, Response
 from scrapy import Spider
 from scrapy.crawler import Crawler
 from typing import Self
+from scrapy import signals
 
 
 class ZenDownloadHandler:
     lazy = False
-    
+
     def __init__(self, crawler: Crawler):
         self.playwright_handler = ScrapyPlaywrightDownloadHandler.from_crawler(crawler)
         self.zyte_handler = ScrapyZyteAPIDownloadHandler.from_crawler(crawler)
@@ -20,8 +21,10 @@ class ZenDownloadHandler:
 
     @classmethod
     def from_crawler(cls, crawler: Crawler) -> Self:
-        return cls(crawler)
-
+        h = cls(crawler)
+        crawler.signals.connect(h.spider_closed, signal=signals.spider_closed)
+        return h
+    
     def download_request(self, request: Request, spider: Spider) -> Deferred | Deferred[Response]:
         if request.meta.get('playwright'):
             return self.playwright_handler.download_request(request, spider)
@@ -31,3 +34,9 @@ class ZenDownloadHandler:
             return self.zyte_handler.download_request(request, spider)
         else:
             return self.scrapy_default_handler.download_request(request, spider)
+
+    async def spider_closed(self, spider: Spider) -> None:
+        self.impersonate_handler.close()
+        self.scrapy_default_handler.close()
+        self.playwright_handler.close()
+        self.zyte_handler.close()
