002f90720672693a90fc99fc7df85bbb8de5635f
Ehsan U. <lancerf562@gmail.com>
2025-06-19 09:11:22 +0500

    add: password

--- scrapy_zen/databases.py
+++ scrapy_zen/databases.py
@@ -33,13 +33,14 @@ class DB(ABC):
 
 
 class RedisDB(DB):
-    settings: List[str] = ["DB_HOST", "DB_PORT"]
+    settings: List[str] = ["DB_HOST", "DB_PORT", "DB_PASS"]
     PROCESSED_IDS_ZSET: str = "processed:ids:zset"
 
-    async def connect(self, host: str = "localhost", port: int = 6379) -> None:
+    async def connect(self, host: str = "localhost", port: int = 6379, password: str = None) -> None:
         self.r = redis.Redis(
             host=host,
             port=port,
+            password=password,
             socket_timeout=5,
             socket_connect_timeout=5,
             decode_responses=True,
@@ -55,11 +56,11 @@ class RedisDB(DB):
         unique_id = f"{spider_name}_{id}"
         score = await self.r.zscore(self.PROCESSED_IDS_ZSET, unique_id)
         return score is not None
-    
+
     async def remove(self, id: str, spider_name: str) -> None:
         unique_id = f"{spider_name}_{id}"
         self.r.delete(unique_id)
-        
+
     async def cleanup(self, days: int) -> None:
         threshold_timestamp = int(time.time()) - (days * 24 * 60 * 60)
         await self.r.zremrangebyscore(self.PROCESSED_IDS_ZSET, 0, threshold_timestamp)
@@ -67,5 +68,3 @@ class RedisDB(DB):
     async def close(self) -> None:
         if hasattr(self, "r"):
             await self.r.aclose()
-
-
